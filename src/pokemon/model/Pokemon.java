8534ecda5eac17e12ef77f127dc88296c858de6d
smeth194243 <smor7432@740s-pl-114.local>
2016-12-01 13:50:36 -0700

    Did things

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -4,5 +4,64 @@ public abstract class Pokemon
 {
 	private int healthPoints;
 	private int combatPoints;
-	private 
+	private double speed;
+	private int number;
+	private String name;
+	
+	
+	public String getPokemonTypes()
+	{
+		return ----;
+	}
+	
+	public String getPokemonInformation()
+	{
+		return ----;
+	}
+	
+	public int getHealthPoints()
+	{
+		return ----;
+	}
+	
+	public int getAttackPoints()
+	{
+		return ----;
+	}
+	
+	public double getSpeed()
+	{
+		return ----;
+	}
+	
+	public int getNumber()
+	{
+		return ----;
+	}
+	
+	public String getName()
+	{
+		return ----;
+	}
+	
+	public void setHealthPoints(int healthPoints)
+	{
+		return ----;
+	}
+	
+	public void setAttackPoints(int attackPoints)
+	{
+		return ----;
+	}
+	
+	public void setSpeed(double speed)
+	{
+		return ----;
+	}
+	
+	public void setName(String name)
+	{
+		return ----;
+	}
+	
 }
