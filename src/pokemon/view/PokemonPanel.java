06d06c53bd750a6a86a1c54dc8aa615cc5090f74
smeth194243 <smor7432@740s-pl-114.local>
2016-12-01 13:20:06 -0700

    Wrote a ton of Pokemon Panel

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,73 @@
 package pokemon.view;
 
-public class PokemonPanel
-{
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.Color;
+import pokemon.controller.PokemonController;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private ImageIcon pokemonIcon;
+	private SpringLayout baseLayout;
+	private JButton updateButton;
+	private JComboBox pokedexSelector;
+	private JLabel pokemonLabel;
+	private JLabel healthLabel;
+	private JLabel combatLabel;
+	private JLabel speedLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel advancedLabel;
+	private JTextField healthField;
+	private JTextField combatField;
+	private JTextField speedField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextArea advancedArea;
+	
+	public PokemonPanel(PokemonController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		//this.pokemonIcon new ImageIcon(getClass().getResource("--------.png"));
+		this.updateButton = new JButton("Update the stats!");
+		this.nameField = new JTextField(25);
+		this.combatField = new JTextField(5);
+		this.healthField = new JTextField(5);
+		this.speedField = new JTextField(5);
+		this.numberField = new JTextField(5);
+		this.advancedArea = new JTextArea(10,25);
+		this.pokedexSelector = new JComboBox(new String [] {"egg", "geodude", "pika", "rattata"});
+		this.advancedLabel = new JLabel("Advanced information:");
+		this.combatLabel = new JLabel("Combat Points:");
+		this.healthLabel = new JLabel("Health Points:");
+		this.speedLabel = new JLabel("Speed Rate:");
+		this.numberLabel = new JLabel("Pokemon Number:");
+		this.nameLabel = new JLabel("My name is:");
+		this.pokemonLabel = new JLabel("The current pokemon", pokemonIcon, JLabel.CENTER);
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	public void setupPanel()
+	{
+		
+	}
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	public void setupListeners()
+	{
+		
+	}
 }
