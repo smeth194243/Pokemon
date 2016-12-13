1cacac375ae1ff98b6ec96315bb55f2b3b0bdc32
smeth194243 <smor7432@740s-pl-114.local>
2016-12-13 13:14:21 -0700

    more charizard

--- src/pokemon/model/Charizard.java
+++ src/pokemon/model/Charizard.java
@@ -1,5 +1,27 @@
 package pokemon.model;
 
-public class Charizard {
-
+public class Charizard extends Pokemon implements Fire, Flying
+{
+	public Charizard()
+	{
+		super("Charizard", 6);
+	}
+	
+	public Charizard(String name)
+	{
+		super(name, 6);
+	}
+	
+	public int Ember()
+	{
+		return 45;
+	}
+	
+	public int FirePunch()
+	{
+		return this.getCombatPoints() * 10;
+	}
+	
+	
+	
 }
